f895bb32ae688f8df9f4e53ddf6de070e97ede6c
Vollkornbrot123 <vollkornbrot123@proton.me>
2024-01-06 17:25:54 +0100

    init

---
+++ src/main/java/com/vollkornbrot/Kniffel/KniffelApplication.java
@@ -0,0 +1,13 @@
+package com.vollkornbrot.Kniffel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniffelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KniffelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vollkornbrot/Kniffel/KniffelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vollkornbrot.Kniffel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KniffelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
